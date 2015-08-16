71b906d16a1929f71f0e84bf9684184203ed2136
monsieursquirrel <conrad@bebbington.org>
2015-08-16 22:48:50 +0100

    make some corrections and start defining midi file header

--- src/step_to_midi.rs
+++ src/step_to_midi.rs
@@ -4,13 +4,24 @@
 const MIDI_SUBDIVISIONS: u8 = 12;
 const STEP_TICKS: u8 = MIDI_SUBDIVISIONS / 4;
 
+const MIDI_FILE_HEADER: [u8; 14] = [
+    'M' as u8,                  // tag
+    'T' as u8,
+    'h' as u8,
+    'd' as u8,
+    0, 0, 0, 6,                 // lengh
+    0, 0,                       // format
+    0, 1,                       // num tracks
+    0, MIDI_SUBDIVISIONS        // divisions
+];
+
 const CHANNEL: u8 = 0;
 const VELOCITY: u8 = 100;
 
 struct MidiDataIter<Steps: Iterator<Item=Vec<u8>>> {
     step_iter: Steps,
     notes_active: Vec<u8>,
-    blank_steps: usize
+    blank_steps: u8
 }
 
 impl<Steps: Iterator<Item=Vec<u8>>> MidiDataIter<Steps> {
@@ -51,7 +62,7 @@ impl<Steps: Iterator<Item=Vec<u8>>> Iterator for MidiDataIter<Steps> {
                 if !notes.is_empty() {
                     for note in notes.iter() {
                         // timestamp
-                        midi_data.push(self.blank_steps * MIDI_SUBDIVISIONS)
+                        midi_data.push(self.blank_steps * STEP_TICKS);
                         // note on message
                         midi_data.push(0x90 | CHANNEL);
                         midi_data.push(*note);
