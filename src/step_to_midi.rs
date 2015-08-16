81a32e9ce0b06a47eecab292a354231d59491908
monsieursquirrel <conrad@bebbington.org>
2015-08-16 23:08:07 +0100

    refactor out notes active vector

--- src/step_to_midi.rs
+++ src/step_to_midi.rs
@@ -20,7 +20,6 @@ const VELOCITY: u8 = 100;
 
 struct MidiDataIter<Steps: Iterator<Item=Vec<u8>>> {
     step_iter: Steps,
-    notes_active: Vec<u8>,
     blank_steps: u8
 }
 
@@ -28,7 +27,6 @@ impl<Steps: Iterator<Item=Vec<u8>>> MidiDataIter<Steps> {
     fn new(step_iter: Steps) -> Self {
         MidiDataIter::<Steps> {
             step_iter: step_iter,
-            notes_active: Vec::new(),
             blank_steps: 0,
         }
     }
@@ -37,46 +35,44 @@ impl<Steps: Iterator<Item=Vec<u8>>> MidiDataIter<Steps> {
 // each step produces a Vec of data bytes that can be flatmapped into a midi file
 impl<Steps: Iterator<Item=Vec<u8>>> Iterator for MidiDataIter<Steps> {
     type Item = Vec<u8>;
+
     fn next(&mut self) -> Option<Self::Item> {
         let this_step = self.step_iter.next();
 
         // end iteration if there's no new notes and no note off events to output
-        if this_step == None && self.notes_active.is_empty() {
-            None
-        }
-        else {
+
+        if let Some(notes) = this_step {
             let mut midi_data = Vec::<u8>::new();
-            // finish previous notes
-            for (i, note) in self.notes_active.iter().enumerate() {
-                // first message is one step after previous, other messages are simultaneous
-                midi_data.push(if i == 0 { STEP_TICKS } else { 0 });
 
-                // note off message
-                midi_data.push(0x80 | CHANNEL);
-                midi_data.push(*note);
-                midi_data.push(VELOCITY);
+            if !notes.is_empty() {
+                for (i, note) in notes.iter().enumerate() {
+                    // differential timestamp - after the first message use 0 for simultaneous
+                    midi_data.push(if i == 0 { self.blank_steps * STEP_TICKS } else { 0 });
+                    // note on message
+                    midi_data.push(0x90 | CHANNEL);
+                    midi_data.push(*note);
+                    midi_data.push(VELOCITY);
+                }
+                for (i, note) in notes.iter().enumerate() {
+                    // first message is one step after previous, other messages are simultaneous
+                    midi_data.push(if i == 0 { STEP_TICKS } else { 0 });
+
+                    // note off message
+                    midi_data.push(0x80 | CHANNEL);
+                    midi_data.push(*note);
+                    midi_data.push(VELOCITY);
+                }
+                self.blank_steps = 0;
             }
-            self.notes_active.clear();
-
-            if let Some(notes) = this_step {
-                if !notes.is_empty() {
-                    for note in notes.iter() {
-                        // timestamp
-                        midi_data.push(self.blank_steps * STEP_TICKS);
-                        // note on message
-                        midi_data.push(0x90 | CHANNEL);
-                        midi_data.push(*note);
-                        midi_data.push(VELOCITY);
-                    }
-                    self.notes_active = notes;
-                    self.blank_steps = 0;
-                }
-                else {
-                    self.blank_steps += 1;
-                }
+            else {
+                self.blank_steps += 1;
             }
 
             Some(midi_data)
         }
+        else {
+            None
+        }
+
     }
 }
