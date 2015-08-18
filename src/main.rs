e2863144bb5c13f80981a784b7618e99aa37bcbb
monsieursquirrel <conrad@bebbington.org>
2015-08-18 20:36:35 +0100

    Argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod step_to_midi;
 mod midi_variable_len;
 
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
 use std::fs::File;
 use std::io::Write;
 
@@ -16,13 +19,43 @@ const SNARE_NOTE: u8 = 38;
 const CH_NOTE: u8 = 42;
 const OH_NOTE: u8 = 46;
 
-fn main() {
-    println!("Hello, world!");
+fn u64_validator(value: String) -> Result<(), String> {
+    value.parse::<u64>().map(|_| () ).map_err(|_| "not a number".to_owned() )
+}
 
-    let outfile = "out.mid";
-    let start = 0;
-    let step = 1;
-    let num_bars: u64 = 8;
+fn main() {
+    let args = App::new("myapp")
+        .version("0.1")
+        .author("monsieursquirrel")
+        .about("Generates every possible beat (for a very crappy drum machine)")
+        .arg(Arg::with_name("START")
+            .help("Sets the pattern number to start from")
+            .required(false)
+            .takes_value(true)
+            .validator(u64_validator)
+            .long("start"))
+        .arg(Arg::with_name("STEP")
+            .help("Sets the pattern number change between each bar")
+            .required(false)
+            .takes_value(true)
+            .validator(u64_validator)
+            .long("step"))
+        .arg(Arg::with_name("NUM_BARS")
+            .help("Sets the number of bars to generate")
+            .required(false)
+            .takes_value(true)
+            .validator(u64_validator)
+            .long("bars"))
+        .arg(Arg::with_name("OUTPUT")
+            .help("Sets the output file to use")
+            .required(true)
+            .index(1))
+        .get_matches();
+
+    let outfile = args.value_of("OUTPUT").unwrap();  // should have been rejected if blank
+    let start: u64 = args.value_of("START").and_then(|arg_str| arg_str.parse().ok() ).unwrap_or(0);
+    let step: u64 = args.value_of("STEP").and_then(|arg_str| arg_str.parse().ok() ).unwrap_or(1);
+    let num_bars: u64 = args.value_of("NUM_BARS").and_then(|arg_str| arg_str.parse().ok() ).unwrap_or(128);
 
     // build a big note sequence
     let mut note_sequence = Vec::<Vec<u8>>::with_capacity(num_bars as usize * 16);
