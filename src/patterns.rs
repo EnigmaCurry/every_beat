6c291181410e1612af951582ab93067f6fc56da4
monsieursquirrel <conrad@bebbington.org>
2015-08-18 21:20:11 +0100

    change the starting pattern to the amen break

--- src/patterns.rs
+++ src/patterns.rs
@@ -15,12 +15,83 @@ const OH_NOTE: u8 = 46;
 const VELOCITY: u8 = 100;
 
 impl InstrumentPattern {
-    fn other_from_u16(num: u16) -> Self {
+    fn kick_from_u16(num: u16) -> Self {
+        // the kick gets its own mappging so that low numbered patterns get a kick at the start of
+        // the bar
+        InstrumentPattern ([
+            ((num & 0x8000) == 0),  // 1
+            ((num & 0x0010) != 0),
+            ((num & 0x0001) == 0),  // and
+            ((num & 0x0020) != 0),
+            ((num & 0x2000) != 0),  // 2
+            ((num & 0x0040) != 0),
+            ((num & 0x0002) != 0),  // and
+            ((num & 0x0080) != 0),
+            ((num & 0x4000) != 0),  // 3
+            ((num & 0x0100) != 0),
+            ((num & 0x0004) == 0),  // and
+            ((num & 0x0200) == 0),
+            ((num & 0x1000) != 0),  // 4
+            ((num & 0x0400) != 0),
+            ((num & 0x0008) != 0),  // and
+            ((num & 0x0800) != 0),
+        ])
+    }
+
+    fn snare_from_u16(num: u16) -> Self {
         // This could be a straightforward mapping of bit positions to pattern positions
         // However, to generate more common patterns first, I'm going to map the lowest bits to the
         // 1/4s, then the 1/8s between them, then finally the raminaing 1/16ths
         InstrumentPattern ([
             ((num & 0x0001) != 0),  // 1
+            ((num & 0x0100) != 0),
+            ((num & 0x0010) != 0),  // and
+            ((num & 0x0200) != 0),
+            ((num & 0x0002) == 0),  // 2
+            ((num & 0x0400) != 0),
+            ((num & 0x0020) != 0),  // and
+            ((num & 0x0800) == 0),
+            ((num & 0x0004) != 0),  // 3
+            ((num & 0x1000) == 0),
+            ((num & 0x0040) != 0),  // and
+            ((num & 0x2000) != 0),
+            ((num & 0x0008) == 0),  // 4
+            ((num & 0x4000) != 0),
+            ((num & 0x0080) != 0),  // and
+            ((num & 0x8000) != 0),
+        ])
+    }
+
+    fn ch_from_u16(num: u16) -> Self {
+        // This could be a straightforward mapping of bit positions to pattern positions
+        // However, to generate more common patterns first, I'm going to map the lowest bits to the
+        // 1/4s, then the 1/8s between them, then finally the raminaing 1/16ths
+        InstrumentPattern ([
+            ((num & 0x0001) != 0),  // 1
+            ((num & 0x0100) == 0),
+            ((num & 0x0010) == 0),  // and
+            ((num & 0x0200) == 0),
+            ((num & 0x0002) == 0),  // 2
+            ((num & 0x0400) == 0),
+            ((num & 0x0020) == 0),  // and
+            ((num & 0x0800) == 0),
+            ((num & 0x0004) == 0),  // 3
+            ((num & 0x1000) == 0),
+            ((num & 0x0040) == 0),  // and
+            ((num & 0x2000) == 0),
+            ((num & 0x0008) != 0),  // 4
+            ((num & 0x4000) == 0),
+            ((num & 0x0080) == 0),  // and
+            ((num & 0x8000) == 0),
+        ])
+    }
+
+    fn oh_from_u16(num: u16) -> Self {
+        // This could be a straightforward mapping of bit positions to pattern positions
+        // However, to generate more common patterns first, I'm going to map the lowest bits to the
+        // 1/4s, then the 1/8s between them, then finally the raminaing 1/16ths
+        InstrumentPattern ([
+            ((num & 0x0001) == 0),  // 1
             ((num & 0x0100) != 0),
             ((num & 0x0010) != 0),  // and
             ((num & 0x0200) != 0),
@@ -32,33 +103,10 @@ impl InstrumentPattern {
             ((num & 0x1000) != 0),
             ((num & 0x0040) != 0),  // and
             ((num & 0x2000) != 0),
-            ((num & 0x0008) != 0),  // 4
+            ((num & 0x0008) == 0),  // 4
             ((num & 0x4000) != 0),
             ((num & 0x0080) != 0),  // and
             ((num & 0x8000) != 0),
-        ])
-    }
-
-    fn kick_from_u16(num: u16) -> Self {
-        // the kick gets its own mappging so that low numbered patterns get a kick at the start of
-        // the bar
-        InstrumentPattern ([
-            ((num & 0x8000) == 0),  // 1
-            ((num & 0x0010) != 0),
-            ((num & 0x0001) != 0),  // and
-            ((num & 0x0020) != 0),
-            ((num & 0x2000) == 0),  // 2
-            ((num & 0x0040) != 0),
-            ((num & 0x0002) != 0),  // and
-            ((num & 0x0080) != 0),
-            ((num & 0x4000) == 0),  // 3
-            ((num & 0x0100) != 0),
-            ((num & 0x0004) != 0),  // and
-            ((num & 0x0200) != 0),
-            ((num & 0x1000) == 0),  // 4
-            ((num & 0x0400) != 0),
-            ((num & 0x0008) != 0),  // and
-            ((num & 0x0800) != 0),
         ])
     }
 }
@@ -67,9 +115,9 @@ impl MachinePattern {
     pub fn from_u64(num: u64) -> Self {
         MachinePattern ([
             InstrumentPattern::kick_from_u16 (((num & 0xffff000000000000) >> 48) as u16),
-            InstrumentPattern::other_from_u16(((num & 0xffff00000000)     >> 32) as u16),
-            InstrumentPattern::other_from_u16(((num & 0xffff0000)         >> 16) as u16),
-            InstrumentPattern::other_from_u16( (num & 0xffff)                    as u16),
+            InstrumentPattern::snare_from_u16(((num & 0xffff00000000)     >> 32) as u16),
+            InstrumentPattern::ch_from_u16   (((num & 0xffff0000)         >> 16) as u16),
+            InstrumentPattern::oh_from_u16   ( (num & 0xffff)                    as u16),
         ])
     }
 
